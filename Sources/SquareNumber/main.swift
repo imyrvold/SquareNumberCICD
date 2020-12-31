5940948175f0a63cd08ee1275b9172b36a4d2b5d
Ivan C Myrvold <ivan@myrvold.org>
2020-12-31 12:08:44 +0100

    replace default main.swift with code

--- Sources/SquareNumber/main.swift
+++ Sources/SquareNumber/main.swift
@@ -1,1 +1,15 @@
-print("Hello, world!")
+import AWSLambdaRuntime
+
+struct Input: Codable {
+    let number: Double
+}
+
+struct Output: Codable {
+    let result: Double
+}
+
+Lambda.run { (context, input: Input, callback: @escaping (Result<Output, Error>) -> Void) in
+    print("input:", input)
+    callback(.success(Output(result: input.number * input.number)))
+}
+
