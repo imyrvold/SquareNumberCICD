9f26b86b9a2eaf06e48cf5272381c19fae31d2af
Ivan C Myrvold <ivan@myrvold.org>
2021-01-01 13:48:39 +0100

    removed Handler not in use

--- Sources/SquareNumber/main.swift
+++ Sources/SquareNumber/main.swift
@@ -11,33 +11,18 @@ struct Output: Codable {
     let result: Double
 }
 
-struct Handler: EventLoopLambdaHandler {
-    typealias In = APIGateway.Request
-    typealias Out = APIGateway.Response
-
-    func handle(context: Lambda.Context, event: In) -> EventLoopFuture<Out> {
-           return context.eventLoop.makeSucceededFuture(APIGateway.Response(
-            statusCode: .ok,
-            headers: [:],
-            multiValueHeaders: nil,
-            body: "Hello",
-            isBase64Encoded: false
-        ))
-    }
-}
-
 struct SquareNumberHandler: EventLoopLambdaHandler {
     typealias In = APIGateway.Request
     typealias Out = APIGateway.Response
     
 
     func handle(context: Lambda.Context, event: In) -> EventLoopFuture<APIGateway.Response> {
+        print("Handler handle event:", event)
         guard let input: Input = try? event.bodyObject() else {
             return context.eventLoop.makeSucceededFuture(APIGateway.Response(with: APIError.requestError, statusCode: .badRequest))
         }
         let output = Output(result: input.number * input.number)
         let apigatewayOutput = APIGateway.Response(with: output, statusCode: .ok)
-        print("Handler handle event:", event)
         
         return context.eventLoop.makeSucceededFuture(apigatewayOutput)
     }
